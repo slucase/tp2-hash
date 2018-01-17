fc66c9082d74c4a14eac9c3287cd326c03723353
Lucas Costa <lucas.costa@engineer.com>
2018-01-17 14:10:35 -0200

    Creates all requested functions

--- main.c
+++ main.c
@@ -7,131 +7,112 @@
 #include <ctype.h>
 
 #define DELIMITER "!? .,-\n"
-#define MAX_N_W 5000
+#define MAX_N_W 20000
 #define MAX_L_W 46 //Comprimento da maior palavra em PT segundo o Google: "pneumoultramicroscopicossilicovulcanoconiótico"
-#define OPTION_DEFAULT '0'
+#define SELECTION_SORT 0
+#define MERGE_SORT 1
 
 
-//STRUCK PARA GERAÇÃO DA HASH TABLE
-typedef struct hTable{
-  int key;
-  char word[MAX_L_W]; //Para caber até a maior palavra da língua portuguesa: 
-  int reps;
-}hashTable;
+//Variaveis globais para os nomes dos arquivos de entrada e saída
+char
+  namein[MAX_L_W], nameout[MAX_L_W];
 
 
 //void  SelectionSort2(hashTable *wordptr[], int n);
 
-void SeqSearch (char unsort_vec[MAX_N_W][MAX_L_W], int n);
+void SeqSearch (char unsort_vec[MAX_N_W][MAX_L_W], int n, int sort_method);
 void SelectionSort (char *wordptr[], int n);
 void MergeSort (char* arr[],int low,int high); //Main MergeSort function
 void Merge (char* arr[],int low,int mid,int high); //Merging the Array Function
-void genHashTable (char unsort_vec[MAX_N_W][MAX_L_W], int n);
+void genHashTable (char unsort_vec[MAX_N_W][MAX_L_W], int n, int sort_method);
 unsigned int hashFunction(const char* str, unsigned int len);
-void OpenTxt (char IN, char *unsort_vec[MAX_N_W][MAX_L_W]);
+int OpenTxt (char *unsort_vec[], int cont_abs);
 
+
+//------------------ MAIN FUNCTION ----------------//
 
 int main(int argc, char **argv){
-  //DECLARAÇÃO DAS VARIAVEIS A SEREM USADAS
-  char *word; //CORRIGIR ESSA PORRA
-  int 
-      i = 0, flag,max,hash = 0, aux = 0, j = 0,h=0, cont_abs=0, reps[MAX_N_W];
   
-  word = malloc(MAX_L_W*sizeof(char));
-
-  //sort using ptr
+  int 
+      i, cont_abs = 0;
+  
   char unsort_vec[MAX_N_W][MAX_L_W]; 
   char *vecptr[MAX_N_W];
 
-  //ABERTURA DO ARQUIVO TXT
-  FILE *IN;
-  IN = fopen (argv[1],"r");
-  //CASO O ARQUIVO NÃO SEJA ABERTO RETORNA O ERRO
+  for (i = 0; i < MAX_N_W; i++)
+    vecptr[i] = unsort_vec[i];
+
+  strcpy(namein, argv[1]);
+  strcpy(nameout, argv[4]);
+
+  cont_abs = OpenTxt (vecptr, 0);
+
+  if (strcmp(argv[2], "0") == 0){
+      if (strcmp(argv[3], "0") == 0) 
+         SeqSearch (unsort_vec, cont_abs, SELECTION_SORT);
+      else
+         SeqSearch (unsort_vec, cont_abs, MERGE_SORT);
+  }
+  else{
+      if (strcmp(argv[3], "0") == 0) 
+         genHashTable (unsort_vec, cont_abs, SELECTION_SORT);
+      else
+         genHashTable (unsort_vec, cont_abs, MERGE_SORT);
+  }
+   
+  return EXIT_SUCCESS;
+}
+//------------------END OF MAIN FUNCTION ----------------//
+
+
+
+
+//--------------------OPEN TXT FUNCTION ----------------//
+
+int OpenTxt (char *unsort_vec[], int cont_abs){
+
+  unsort_vec = malloc((cont_abs)*sizeof(char *));
+
+  char 
+      *word; 
+  word = malloc(MAX_L_W*sizeof(char));
+  
+  int 
+      i;
+  
+  FILE
+      *IN;
+  IN = fopen (namein,"r");
   if(IN == NULL){
-    printf("Arquivo não foi aberto!\n");
+    printf("Error opening the file! Terminating... \n");
     exit (1);
   }
-//POR CUIDADOS VAMOS ZERAR O BUFFER TAMBEM
+
+  //Zerando buffer
   fflush(stdout);
 
   while ((fscanf (IN, "%m[^"DELIMITER"]%*["DELIMITER"]", &word)) != EOF){
-      for(i = 0; word[i]; i++) 
-          word[i] = tolower(word[i]);
-      //VETOR ---UNSORTED---
-      strcpy(unsort_vec[cont_abs], word);
-      cont_abs++;
+//  while ((fscanf (IN, "%s", &word)) != EOF){
+        for(i = 0; word[i]; i++) 
+            word[i] = tolower(word[i]);
+        //VETOR ---UNSORTED---
+        strcpy(unsort_vec[cont_abs], word);
+        (cont_abs)++;
   }
-
-  //genHashTable (unsort_vec, cont_abs);
-  SeqSearch (unsort_vec, cont_abs);
-  
-  //LIMPA A VARIAVEL WORD
   free(word);
-  //FECHA O ARQUIVO
+  free(unsort_vec);
   fclose (IN);
-  return EXIT_SUCCESS;
+  return cont_abs;
 }
 
-//Merge sort function for MergeSort
-void Merge(char* arr[],int low,int mid,int high) //Merging the Array Function
-{
-    int nL= mid-low+1;
-    int nR= high-mid;
+//------------------END OF OPEN TXT FUNCTION ----------------//
 
-    char** L=malloc(sizeof(char *)*nL);
-    char** R=malloc(sizeof(char *)*nR);
-    int i;
-    for(i=0;i<nL;i++)
-    {
-        L[i]=malloc(sizeof(arr[low+i]));
-        L[i]=arr[low+i];
-    }
-    for(i=0;i<nR;i++)
-    {
-        R[i] = malloc(sizeof(arr[mid+i+1]));
-        R[i] = arr[mid+i+1];
-    }
-    int j=0,k;
-    i=0;
-    k=low;
-    while(i<nL&&j<nR)
-    {
-        if(strcmp(L[i],R[j])<0) arr[k++]=L[i++];
-        else arr[k++] = R[j++];
-    }
-    while(i<nL) arr[k++] = L[i++];
-    while(j<nR) arr[k++] = R[j++];
 
-}
-void MergeSort(char* arr[],int low,int high) //Main MergeSort function
-{
-    if(low<high)
-    {
-        int mid=(low+high)/2;
-        MergeSort(arr,low,mid);
-        MergeSort(arr,mid+1,high);
-        Merge(arr,low,mid,high);
-    }
-}
 
-void SelectionSort(char* wordptr[], int n){
-    int i, eff_size, maxpos = 0;
-    char *auxptr;
 
-    for (eff_size = n; eff_size > 1; eff_size--){
-        for (i = 0; i < eff_size; i++){
+//--------------------SEQ  SEARCH  FUNCTION ----------------//
 
-            if (strcmp(wordptr[i], wordptr[maxpos]) > 0)
-                maxpos = i;
-
-        }
-        auxptr = wordptr[maxpos];
-        wordptr[maxpos] = wordptr[eff_size-1];
-        wordptr[eff_size-1] = auxptr;
-    }
-}
-
-void SeqSearch (char unsort_vec[MAX_N_W][MAX_L_W], int n){
+void SeqSearch (char unsort_vec[MAX_N_W][MAX_L_W], int n, int sort_method){
  
   char 
       *word, *vecptr[MAX_N_W]; //RETIRAR ESSA PORRA
@@ -168,23 +149,34 @@ void SeqSearch (char unsort_vec[MAX_N_W][MAX_L_W], int n){
  
  for (i = 0; i < cont_abs; i++)
     vecptr[i] = wordlist[i];
-
- printf("\nCont = %d\nPre sort", cont_abs);
-   for (i = 0; i < cont_abs; i++){
-       printf("\n%s = %d", wordlist[i], reps[i]);
- }
-// SelectionSort(vecptr, cont_abs);
- MergeSort(vecptr, 0, cont_abs-1); //Main MergeSort function
- 
- printf("\n\nCont = %d\nPos sort", cont_abs);
-   for (i = 0; i < cont_abs; i++){
-       j=((vecptr[i]-wordlist[0])/sizeof(wordlist[0]));
-       printf("\n%s = %d  --- i = %d", vecptr[i], reps[j], i);
-   }
   
+  //SORT METHOD
+  if (sort_method == 0)
+     SelectionSort(vecptr, cont_abs);
+  if (sort_method != 0)
+     MergeSort(vecptr, 0, cont_abs-1);
+
+  //Print file
+  FILE 
+      *OUT;
+  OUT = fopen(nameout, "w");
+  
+  for (i = 0; i < cont_abs; i++){
+      j=((vecptr[i]-wordlist[0])/sizeof(wordlist[0]));
+      fprintf (OUT, "%s %d\n", vecptr[i], reps[j]);
+  }
+  fclose(OUT);
+  free(word);
 }
 
-void genHashTable (char unsort_vec[MAX_N_W][MAX_L_W], int n){
+//------------------END OF SEQ SEARCH FUNCTION ----------------//
+
+
+
+
+//-------------------- HASH SEARCH FUNCTION ----------------//
+
+void genHashTable (char unsort_vec[MAX_N_W][MAX_L_W], int n, int sort_method){
 
   char 
       *word, *vecptr[MAX_N_W]; //RETIRAR ESSA PORRA
@@ -192,7 +184,7 @@ void genHashTable (char unsort_vec[MAX_N_W][MAX_L_W], int n){
   word = malloc(MAX_L_W*sizeof(char));
  
   
-  char words[MAX_N_W][MAX_L_W];
+  char wordlist[MAX_N_W][MAX_L_W];
 
   int
       i, hash = 0, aux = 0, j = 0, cont_abs, reps[MAX_N_W], key[MAX_N_W]; 
@@ -210,21 +202,21 @@ void genHashTable (char unsort_vec[MAX_N_W][MAX_L_W], int n){
       hash = hashFunction(word, strlen(word));
       aux = hash;
       if (reps[hash] == 0){
-        strcpy(words[hash], word);
+        strcpy(wordlist[hash], word);
         reps[hash]++;
-      }else if(strcmp(words[hash], word) == 0){
+      }else if(strcmp(wordlist[hash], word) == 0){
         reps[hash]++;
         cont_abs--;
       }else if (key[hash] == 0){
         while (reps[hash] != 0){
           hash++;
         }
-        strcpy(words[hash], word);
+        strcpy(wordlist[hash], word);
         reps[hash] ++;
         key[hash]++;
       }else if(key[hash] != 0){
         for (i = 0;i < MAX_N_W; i++){
-          if (strcmp(words[i], word) == 0){
+          if (strcmp(wordlist[i], word) == 0){
             reps[i]++;
             cont_abs--;
             break;
@@ -234,26 +226,41 @@ void genHashTable (char unsort_vec[MAX_N_W][MAX_L_W], int n){
         while (key[hash] != 0){
           hash++;
         }
-        strcpy(words[hash], word);
+        strcpy(wordlist[hash], word);
         key[hash]++;
       }
     }
   
-  printf ("contabs = %d \n", cont_abs);
-  
   for (i = 0; i < MAX_N_W; i++){
   			if (reps[i] > 0)
-                vecptr[j++] = words[i]; 
+                vecptr[j++] = wordlist[i]; 
   }
+  
+  //SORT METHOD
+  if (sort_method == 0)
+     SelectionSort(vecptr, cont_abs);
+  if (sort_method != 0)
+     MergeSort(vecptr, 0, cont_abs-1);
 
- SelectionSort(vecptr, cont_abs);
- //MergeSort(vecptr, 0, cont_abs-1); //Main MergeSort function
-
+  //Print file
+  FILE 
+      *OUT;
+  OUT = fopen(nameout, "w");
+  
   for (i = 0; i < cont_abs; i++){
-            printf(" %s\n", vecptr[i]); 
+      j=((vecptr[i]-wordlist[0])/sizeof(wordlist[0]));
+      fprintf (OUT, "%s %d\n", vecptr[i], reps[j]);
   }
+  fclose(OUT);
   free(word);
 }
+
+//------------------END OF HASH SEARCH FUNCTION ----------------//
+
+
+
+
+//-------------------- HASH GEN FUNCTION ----------------//
 
 unsigned int hashFunction(const char* str, unsigned int len){
   const unsigned int resp = 2;
@@ -265,3 +272,86 @@ unsigned int hashFunction(const char* str, unsigned int len){
   }
   return hash%MAX_N_W;
 }
+//-------------------- END OF HASH GEN FUNCTION ----------------//
+
+
+
+
+
+//--------------------MERGE  FUNCTION ----------------//
+
+  void Merge(char* arr[],int low,int mid,int high) //Merging the Array Function
+{
+    int nL= mid-low+1;
+    int nR= high-mid;
+
+    char** L=malloc(sizeof(char *)*nL);
+    char** R=malloc(sizeof(char *)*nR);
+    int i;
+    for(i=0;i<nL;i++)
+    {
+        L[i]=malloc(sizeof(arr[low+i]));
+        L[i]=arr[low+i];
+    }
+    for(i=0;i<nR;i++)
+    {
+        R[i] = malloc(sizeof(arr[mid+i+1]));
+        R[i] = arr[mid+i+1];
+    }
+    int j=0,k;
+    i=0;
+    k=low;
+    while(i<nL&&j<nR)
+    {
+        if(strcmp(L[i],R[j])<0) arr[k++]=L[i++];
+        else arr[k++] = R[j++];
+    }
+    while(i<nL) arr[k++] = L[i++];
+    while(j<nR) arr[k++] = R[j++];
+    free(L);
+    free(R);
+}
+//------------------END OF MERGE FUNCTION ----------------//
+
+
+
+
+//--------------------MERGE SORT  FUNCTION ----------------//
+
+void MergeSort(char* arr[],int low,int high) //Main MergeSort function
+{
+
+    if(low<high)
+    {
+        int mid=(low+high)/2;
+        MergeSort(arr,low,mid);
+        MergeSort(arr,mid+1,high);
+        Merge(arr,low,mid,high);
+    }
+}
+
+//------------------END OF MERGE SORT FUNCTION ----------------//
+
+
+
+
+//--------------------SELECT  SORT  FUNCTION ----------------//
+
+void SelectionSort(char* wordptr[], int n){
+    int i, eff_size, maxpos = 0;
+    char *auxptr;
+    
+    for (eff_size = n; eff_size > 1; eff_size--){
+        for (i = 0; i < eff_size; i++){
+
+            if (strcmp(wordptr[i], wordptr[maxpos]) > 0)
+                maxpos = i;
+
+        }
+        auxptr = wordptr[maxpos];
+        wordptr[maxpos] = wordptr[eff_size-1];
+        wordptr[eff_size-1] = auxptr;
+    }
+}
+
+//------------------END OF SELECT SORT FUNCTION ----------------//
